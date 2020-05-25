ea1926057845484422f4176852f2c6018600cfbc
leighton110 <lcag917@gmail.com>
2020-05-25 13:33:43 +0900

    add hospital login

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -3,23 +3,39 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('../config/config')[process.env.NODE_ENV || 'development'];
 
-const authCheck = async (req, res, next) => {
+const jwtCheck = async (req, res, next) => {
   const authHeader = req.cookies['Authorization'];
   console.log(authHeader);
   if (authHeader) {
     const token = authHeader.replace('Bearer ', '');
-    const { username } = jwt.verify(token, config.jwtSecretKey);
-    const findUser = await model.User.findOne({
+    const { type, username } = jwt.verify(token, config.jwtSecretKey);
+    return { userType: type, username };
+  } else {
+    return { userType: null, username: null };
+  }
+};
+
+const authCheck = async (req, res, next) => {
+  const { userType, username } = await jwtCheck(req, res, next);
+  let findUser;
+  if (userType === 'user') {
+    findUser = await model.User.findOne({
       where: { username },
     });
-    if (findUser) {
-      req.findUserId = findUser.id;
-      next();
-    } else {
-      res.json({ result: '해당 아이디가 없습니다.' });
-    }
+  } else if (userType === 'hospital') {
+    findUser = await model.Hospital.findOne({
+      where: { username },
+    });
   } else {
     res.json({ result: '로그인을 해주세요' });
+  }
+
+  if (findUser) {
+    req.findUserId = findUser.id;
+    req.userType = userType;
+    next();
+  } else {
+    res.json({ result: '해당 아이디가 없습니다.' });
   }
 };
 
--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,13 +1,45 @@
 const model = require('../models');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const config = require('../config/config')[process.env.NODE_ENV || 'development'];
 
 const hospital = async (req, res, next) => {
   try {
-    console.log('병원 로그인요청을 받았습니다.');
-    const { username, pwd } = req.body;
-    console.log(username, pwd);
-    res.status(200).json({ username });
-  } catch (error) {
-    console.log(error);
+    const { username, password } = req.body;
+    const findHospital = await model.Hospital.findOne({
+      where: { username },
+    });
+    if (!findHospital) {
+      console.log('err');
+      //아이디없음
+    }
+    console.log();
+    if (bcrypt.compareSync(password, findHospital.password)) {
+      console.log('비밀번호 같음');
+      //여기서부터 jwt로 토큰만들기
+      const options = {
+        algorithm: 'HS256',
+        expiresIn: 60 * 60 * 24 * 30,
+      };
+
+      const payload = {
+        iss: 'myDomain',
+        username,
+        type: 'hospital',
+        name: findHospital.title,
+      };
+      console.log(payload);
+      const token = await jwt.sign(payload, config.jwtSecretKey, options);
+      console.log('token' + token);
+      res.cookie('Authorization', `Bearer ${token}`);
+      res.status(200).json({
+        result: '로그인 성공',
+      });
+    } else {
+      res.json({ result: '비밀번호 틀렸어' });
+    }
+  } catch (err) {
+    console.log(err);
   }
 };
 
@@ -32,6 +64,7 @@ const user = async (req, res, next) => {
       const payload = {
         iss: 'myDomain',
         username,
+        type: 'user',
         name: findUser.name,
       };
       const token = await jwt.sign(payload, config.jwtSecretKey, options);
